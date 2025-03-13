21add16d7c4f5b03398d8f578c06ecac1882c1f1
Jamie Schmitt <jamie.schmitt@gmail.com>
2025-03-13 15:14:30 -0700

    added icon

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from controller import FinanceController
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 import sys
 
 # Run the application
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    #app.setStyle("cleanlooks")
+    
     app.setStyle("Fusion")
+    app.setWindowIcon(QIcon('images/logo.webp'))
+    
     window = FinanceController()
     window.show()
     sys.exit(app.exec())
