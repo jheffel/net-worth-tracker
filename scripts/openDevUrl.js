08b45454dd57cf35a49096508cb61eb14d88442b
Jamie Schmitt <jamie.schmitt@gmail.com>
2026-07-03 09:45:55 -0700

    license update

--- scripts/openDevUrl.js
+++ scripts/openDevUrl.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
+
+// Net Worth Tracker
+// Copyright (C) 2025 jheffel
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 // Small helper: wait a bit then open the dev app at the correct path
 const { exec } = require('child_process');
 
